8610477db71a89060f78ce0db319cb6f9314ee56
mac <luckysevenbear@gmail.com>
2017-02-08 11:55:21 +0200

    step-2

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -8,6 +8,25 @@ import style from "./app.component.scss";
   styles: [ style ]
 })
 export class AppComponent {
+  parties: any[];
+
   constructor() {
+    this.parties = [
+      {
+        'name': 'Dubstep-Free Zone',
+        'description': 'Can we please just for an evening not listen to debstep.',
+        'location': 'Palo Alto'
+      },
+      {
+        'name': 'All dubstep all the time',
+        'description': 'Get it on!',
+        'location': 'Palo Alto'
+      },
+      {
+        'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.',
+        'location': 'San Francisco'
+      },
+    ];
   }
 }
