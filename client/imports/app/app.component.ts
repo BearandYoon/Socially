a9bb4e3f628722c44c59b87d0c8fad0d995a5ec6
mac <luckysevenbear@gmail.com>
2017-02-08 16:35:48 +0200

    step-3

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from "@angular/core";
-import template from "./app.component.html";
-import style from "./app.component.scss";
+import { Component }    from "@angular/core";
+import { Parties }      from '../../../both/collections/parties.collection';
+import { Party }        from '../../../both/models/party.model';
+import { Observable }   from 'rxjs/Observable';
+import template         from "./app.component.html";
+import style            from "./app.component.scss";
 
 @Component({
   selector: "app",
@@ -8,25 +11,9 @@ import style from "./app.component.scss";
   styles: [ style ]
 })
 export class AppComponent {
-  parties: any[];
+  parties: Observable<Party[]>;
 
   constructor() {
-    this.parties = [
-      {
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to debstep.',
-        'location': 'Palo Alto'
-      },
-      {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      },
-    ];
+    this.parties = Parties.find({}).zone();
   }
 }
